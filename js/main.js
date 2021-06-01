bfde6deca8237ee76a745da11fac3b3eb138452c
Neetesh Kuchya <38105608+kuchya@users.noreply.github.com>
2021-06-01 19:26:01 +0530

    list added for todos
    list added for todos

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const taskNameElement = document.querySelector('#taskname');
 const taskDateElement = document.querySelector('#taskdate');
 const taskTimeElement = document.querySelector('#tasktime');
+const taskListElement = document.querySelector('#taskList');
 const AllTasks = [];
 
 function addTask() {
@@ -18,6 +19,24 @@ function addTask() {
         taskNameElement.value = "";
         taskDateElement.value = "";
         taskTimeElement.value = "";
-        console.log(AllTasks);
+        alert("Task Added")
+        // refreshList();
     }
 }
+
+function refreshList() {
+    taskListElement.innerHTML = "";
+    for (let i = 0; i < AllTasks.length; i++) {        
+        taskListElement.innerHTML += `<li class="list-group-item">
+                                            <span class="float-right badge badge-danger">
+                                                &times;
+                                            </span>
+                                            <b>` + AllTasks[i].name + `</b> <br/>
+                                            <b>Date :</b> ` + AllTasks[i].date + ` <br/>
+                                            <b>Time :</b> ` + AllTasks[i].time + `
+                                        </li>`;
+    }
+}
+
+// Page Setup
+refreshList()
